32814ab46c97181141d0c88643f8035b85e1a8f4
David King <40282634+GradHatter@users.noreply.github.com>
2024-01-10 18:53:34 -0500

    Update Comparison.py
    Fixed search algorithm to include flipping hat
    added comments

--- Comparison.py
+++ Comparison.py
@@ -23,90 +23,104 @@ world = pd.read_csv(filename)
 #read in hat outline csv
 hat = pd.read_csv("T:\Hat_Outline\Hat_outline.csv")
 
+#-------------------------------------------------------------------------------------
+#normalize and center the hat
+
 #find area of the hat
 hat_raw_poly = Polygon(list(zip(hat['x'],hat['y'])))
 hat_area = hat_raw_poly.area
-
 
 #normalize hat area to 1
 hat['x_norm'] = hat['x']*np.sqrt(1/(hat_area))
 hat['y_norm'] = hat['y']*np.sqrt(1/(hat_area))
 
+#find the center of the normalized hat
 hat_norm_poly = Polygon(list(zip(hat['x_norm'],hat['y_norm'])))
 hat_center = np.array(hat_norm_poly.centroid.coords)[0]
 
-#Check it is normalized
-#print(PolyArea(hat['x_norm'],hat['y_norm']))
-
+#center the hat
 hat['x_cent'] = hat['x_norm'] - hat_center[0]
 hat['y_cent'] = hat['y_norm'] - hat_center[1]
 
-#make list of tuples from dataframe
-column1 = 'x_cent'
-column2 = 'y_cent'
-hat_list = list(zip(hat[column1], hat[column2]))
-
-hat_polygon = Polygon(hat[[column1,column2]])
-
-#rotate hat    
-#hat_polygon = affinity.rotate(hat_polygon, 70) #Canada
-#hat_polygon = affinity.rotate(hat_polygon, 75) #Russia
-#hat_polygon = affinity.rotate(hat_polygon, 120) #Mexico
-
-#flip Polygon
-#hat_polygon = transform(lambda x, y, z=None: (x, -y), hat_polygon) #Mexico
+#make the centered hat coordinates into a polygon
+hat_polygon = Polygon(list(zip(hat['x_cent'],hat['y_cent'])))
 
 #--------------------------------------------------------------------------------
 def norm_cent_poly(poly, country = None):
-    c_area = poly.area
+    c_area = poly.area #find area of the polygon
+
+    #makes 2 rectangles on the + and - boundries
+    #goes from noth pole but stops at -75deg long to exclude antartica
+    side1 = Polygon([(180,90), (180,-75), (179,-75), (179,90)])
+    side2 = Polygon([(-180,90), (-180,-75), (-179,-75), (-179,90)])
+
+    #check if polygon wraps around the international dateline
+    if poly.intersects(side1) and poly.intersects(side2):
+        edge_overlap = True
+    else:
+        edge_overlap = False
 
     blobs = []
     if poly.geom_type == 'MultiPolygon':
         for i in range(len(poly.geoms)):
+            #splits each subpolygon into x and y coordinates
             x,y = poly.geoms[i].exterior.xy
             x_corr = np.array(x)
             y_corr = np.array(y)
-            if country == "Russia":
+            #fixes countries arbitrarily split by the international dateline
+            if edge_overlap is True:
                 x_corr = np.where(x_corr < 0, x_corr+360, x_corr)
             
+            #normalize country area to 1
             cx_norm = x_corr*np.sqrt(1/(c_area))
             cy_norm = y_corr*np.sqrt(1/(c_area))
 
             blobs.append(Polygon(list(zip(cx_norm, cy_norm))))
 
+        #Find the center of the normalized country
         country_norm = MultiPolygon(blobs)
-
         cent = np.array(country_norm.centroid.coords)[0]
 
         blobs = []
-
         for i in range(len(country_norm.geoms)):
+            #splits each subpolygon into x and y coordinates
             x,y = country_norm.geoms[i].exterior.xy
             x = np.array(x)
             y = np.array(y)
 
+            #centers normalized country shape
             cx_cent = x - cent[0]
             cy_cent = y - cent[1]
 
             blobs.append(Polygon(list(zip(cx_cent, cy_cent))))
 
+        #makes the centered and normalized country into one multipolygon
         country_blob = MultiPolygon(blobs)
 
     elif poly.geom_type == 'Polygon':
+        #splits polygon into x and y coordinates
         x,y = poly.exterior.xy
         x_corr = np.array(x)
         y_corr = np.array(y)
+
+        #fixes countries that cross the international dateline
+        if edge_overlap is True:
+                x_corr = np.where(x_corr < 0, x_corr+360, x_corr)
             
+        #normalizes country area to 1
         cx_norm = x_corr*np.sqrt(1/(c_area))
         cy_norm = y_corr*np.sqrt(1/(c_area))
 
+        #find normalized country's center
         country_norm = Polygon(list(zip(cx_norm, cy_norm)))
-
         cent = np.array(country_norm.centroid.coords)[0]
 
+        #splits normalized polygon into x and y coordinates
         x,y = country_norm.exterior.xy
         x = np.array(x)
         y = np.array(y)
+
+        #center polygon to 0,0
 
         cx_cent = x - cent[0]
         cy_cent = y - cent[1]
@@ -121,6 +135,7 @@ def find_similarity_score(hat_polygon, country_poly, show = False):
 
     unique_hat_poly = hat_polygon.difference(country_poly)
 
+    #total hat area-remaining hat area = % overlap
     similarity_score = 100*(1-unique_hat_poly.area)
 
     if show is True:
@@ -146,6 +161,7 @@ def plot_poly(poly, shape = "exterior", color = 'green', name = None):
                                         mode = "lines", 
                                         fill = 'toself', fillcolor=color,
                                         line = {'color' : color}, name = name))
+            #plot the holes (ideally in the same color as the background)
             elif shape == "interior":
                 for inner in geom.interiors:
                     xs, ys = inner.xy
@@ -164,7 +180,7 @@ def plot_poly(poly, shape = "exterior", color = 'green', name = None):
                                         mode = "lines", 
                                         fill = 'toself', fillcolor=color,
                                         line = {'color' : color}, name = name))
-            
+        #plot the holes (ideally in the same color as the background)    
         elif shape == "interior":
             xs, ys = inner.xy
 
@@ -179,6 +195,7 @@ def plot_poly(poly, shape = "exterior", color = 'green', name = None):
     
 #--------------------------------------------------------------------------------
 
+#initialize columns of all zeros
 world['Similarity Score'] = 0
 world['Subtracted Hat Poly'] = 0
 world['Norm Cent Country'] = 0
@@ -187,33 +204,48 @@ world['Flip'] = 0
 
 similarity_dict = {}
 country_list = world['name'].tolist()
-#hat_polygon = transform(lambda x, y, z=None: (x, -y), hat_polygon)
 
 for country in country_list:
+    #get country geometry then normalize and center
     c = world[(world.name == country)]['geometry'].item()
     c = wkt.loads(c)
     country_poly = norm_cent_poly(c, country == country)
-    #rotate the hat every 10 degrees and check overlap
+
+    #rotate the hat every 5 degrees and check overlap
+    #keep track of conditions resulting in the highest overlap percentage
     best = [0,0,0,0] #[unique_hat_poly, similarity_score, i*10, flip]
+    #rotate the hat every 5 degrees and check overlap
     for i in range(0,360,5):
         hat_polygon = affinity.rotate(hat_polygon, i)
+        #flip the hat along the y axis and check again for overlap
         for flip in [True,False]:
             if flip is True:
                 hat_polygon = transform(lambda x, y, z=None: (x, -y), hat_polygon)
-            elif flip is False:
-                pass
-            else:
-                print("error, flip neither True or False")
 
-        unique_hat_poly, similarity_score = find_similarity_score(hat_polygon,
-                                                                country_poly,
-                                                                show = False)
-        
-        if similarity_score > best[1]:
-            best = [unique_hat_poly, similarity_score, i, flip]
-        else:
-            pass
-    
+                #get non-overlapping hat polygon and similarity percentage as decimal
+                unique_hat_poly, similarity_score = find_similarity_score(hat_polygon,
+                                                                        country_poly,
+                                                                        show = False)
+                
+                #if current conditions are better than the previous best, update best
+                if similarity_score > best[1]:
+                    best = [unique_hat_poly, similarity_score, i, flip]
+                else:
+                    pass
+
+            elif flip is False:
+                #get non-overlapping hat polygon and similarity percentage as decimal
+                unique_hat_poly, similarity_score = find_similarity_score(hat_polygon,
+                                                                        country_poly,
+                                                                        show = False)
+                
+                #if current conditions are better than the previous best, update best
+                if similarity_score > best[1]:
+                    best = [unique_hat_poly, similarity_score, i, flip]
+                else:
+                    pass
+
+    #update world df with best conditions
     world.loc[(world.name == country),
                 'Norm Cent Country'] = country_poly
     world.loc[(world.name == country),
@@ -227,10 +259,15 @@ for country in country_list:
 
 #unique_hat_poly, similarity_score = find_similarity_score(hat_polygon, country_poly)
 
-print(world.loc[world['Similarity Score'].idxmax()])
+max_overlap = world.loc[world['Similarity Score'].idxmax()]
+min_overlap = world.loc[world['Similarity Score'].idxmin()]
 
-country = world.loc[world['Similarity Score'].idxmax(), 'name']
+#display the countries with the most and least overlap
+print(f"{max_overlap['name']} is the most hat-like country with a similarity of {max_overlap['Similarity Score']:,.2f}%")
+print(f"{min_overlap['name']} is the least hat-like country with a similarity of {min_overlap['Similarity Score']:,.2f}%")
 
+
+country = max_overlap['name']
 #unique_hat_poly = wkt.loads(world[(world.name == country)]['Subtracted Hat Poly'].item())
 unique_hat_poly = world[(world.name == country)]['Subtracted Hat Poly'].item()
 country_poly = world[(world.name == country)]['Norm Cent Country'].item()
@@ -238,7 +275,7 @@ country_poly = world[(world.name == country)]['Norm Cent Country'].item()
 world.to_csv("world_similarity.csv")
 
 #-----------------------------------------------------------------------------
-
+#plot the country and the non-overlapping portions of the hat
 fig = go.Figure().update_layout(plot_bgcolor='white')
 
 plot_poly(unique_hat_poly, shape = 'exterior', color = 'orange', name = "Hat")
